854912bb7bc426d5788ba56faab9d2f725c37a72
coolbot100s <76798835+coolbot100s@users.noreply.github.com>
2022-05-29 23:35:49 -0700

    Update mod.lua
    Changed mod_id to no longer contain invalid characters lol.
    
    Also greatly extended "make_variated" so it's useful in a bunch of scenarios where you want variations of an object now.

--- mod.lua
+++ mod.lua
@@ -1,5 +1,5 @@
 -- https://github.com/coolbot100s/decor-more
-MOD_NAME = "decor&more"
+MOD_NAME = "decornmore"
 
 function register()
     return {
@@ -10,23 +10,47 @@ function register()
 end
 function init()
     api_log("init", "ayo we did it")
-    register_variated_objects()
     return "Success"
 end
 
 COLORS = {"red","blue","yellow","green","orange","purple","black","white","gold","turqoise","lime","pink","brown"}
 
-variated_objects_generics = {{}} --contains object deifinitions for things that should recieve color variations
+variated_objects_generics = {{"object definition table goes here"}} --contains object deifinitions for things that should recieve color variations.
 
-function register_variated_objects() -- these could be one function? idk this might look better.
-    for i = 1,#variated_objects_generics do
-        make_variated(variated_objects_generics[i])
+--- Returns a table of tables that contain object_definitions and sprites with desired variations that can then be used like so: api_define_object(returned_val[i][1], returned_val[i][2])
+-- @param obj_def An object_definition that contains a non-variated version of the item.
+-- @param variations List of variations as a table with strings, example: COLORS
+-- @param folder A string with the name of the folder the spritesheets are found in.
+-- @param edit_tooltip Bool to determine if the tooltip should be editid for each variation or not. If false tooltip will be the same for all variations
+-- @param tooltip[opt] a dictionary containing 3 items "preface" "insert" "suffix". preface will come before the insert (changes on variation) part of the tooltip, "suffix" will come after. "insert" should contain a space on either side of the text you want to display, example: " Hello World " and NOT in preface or suffix. 
+function make_variated( obj_def, variations, folder, edit_tooltip, tooltip)
+    variation_list = {}
+    for i = 1,#variations do
+        obj_def["id"] = variations[i] .. "_" .. obj_def["id"]               
+        obj_def["name"] = up(variations[i]) .. " " .. obj_def["name"]
+        if edit_tooltip == true then
+            if tooltip["insert"] ~= nil then
+                obj_def["tooltip"] = tooltip["preface"] .. tooltip["insert"] .. tooltip["suffix"]
+            else
+                obj_def["tooltip"] = tooltip["preface"] .. " " .. up(variations[i]) .. " " .. tooltip["suffix"]
+            end
+        sprite_path = folder .. "/" .. obj_def["id"] .. "/" .. obj_def["id"] .. "_sheet.png"
+        table.insert({obj_def, sprite_path})
+        end
+    return variation_list
     end
 end
-function make_variated(obj) -- pass a normal object definition
-    for i = 1,#COLORS do
-        obj["id"] = COLORS[i] .. "_" .. obj["id"]
-        obj["name"] = up(COLORS[i]) .. " " .. obj["name"]
-        api_define_object(obj, "sprites/" .. obj[id .. "_sheet.png"])
+
+function define_from_variation_list(variation_list)
+    for i = 1,#variation_list do
+        api_define_object(variation_list[i][1], variation_list[i][2])
     end
+end
+
+--EXAMPLE USAGE
+function define_my_furniture()
+    COLORS = {"red","blue","yellow","green","orange","purple","black","white","gold","turqoise","lime","pink","brown"}
+    my_furniture_definition = {"object definition here"}
+    tooltip = {preface = "This is a", suffix = "piece of furniture by Me"}
+    define_from_variation_list(make_variated(my_furniture_definition, COLORS, "my_furniture", 1, tooltip))
 end
