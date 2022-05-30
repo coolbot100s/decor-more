3fb6476480b3628fe22a9b2d77b004294374ded8
coolbot100s <76798835+coolbot100s@users.noreply.github.com>
2022-05-30 00:37:54 -0700

    sprite_path()
    --- Generates and returns a path to the sprite of an object as a string
    ---@param name string typically object_definition["id"]
    ---@param folder string (Optional) Subfolder for the sprite, if none is given will use name instead.

--- mod.lua
+++ mod.lua
@@ -17,12 +17,12 @@ COLORS = {"red","blue","yellow","green","orange","purple","black","white","gold"
 
 variated_objects_generics = {{"object definition table goes here"}} --contains object deifinitions for things that should recieve color variations.
 
---- Returns a table of tables that contain object_definitions and sprites with desired variations that can then be used like so: api_define_object(returned_val[i][1], returned_val[i][2])
--- @param obj_def An object_definition that contains a non-variated version of the item.
--- @param variations List of variations as a table with strings, example: COLORS
--- @param folder A string with the name of the folder the spritesheets are found in.
--- @param edit_tooltip Bool to determine if the tooltip should be editid for each variation or not. If false tooltip will be the same for all variations
--- @param tooltip[opt] a dictionary containing 3 items "preface" "insert" "suffix". preface will come before the insert (changes on variation) part of the tooltip, "suffix" will come after. "insert" should contain a space on either side of the text you want to display, example: " Hello World " and NOT in preface or suffix. 
+--- Returns a table of tables that contain obj_definitions and sprites with desired variations that can then be used like so: api_define_object(returned_val[i][1], returned_val[i][2])
+---@param obj_def obj_definition An obj_definition that contains a non-variated version of the item.
+---@param variations table List of variations as a table with strings, example: COLORS
+---@param folder string Name of the folder the spritesheets are found in.
+---@param edit_tooltip boolean If the tooltip should be editid for each variation or not. If false tooltip will be the same for all variations
+---@param tooltip table [Optional] a dictionary containing 3 items "preface" "insert" "suffix". Preface will come before the insert (changes on variation) part of the tooltip, "suffix" will come after. "insert" should contain a space on either side of the text you want to display, example: " Hello World " and NOT in preface or suffix. 
 function make_variated( obj_def, variations, folder, edit_tooltip, tooltip)
     variation_list = {}
     for i = 1,#variations do
@@ -34,7 +34,7 @@ function make_variated( obj_def, variations, folder, edit_tooltip, tooltip)
             else
                 obj_def["tooltip"] = tooltip["preface"] .. " " .. up(variations[i]) .. " " .. tooltip["suffix"]
             end
-        sprite_path = folder .. "/" .. obj_def["id"] .. "/" .. obj_def["id"] .. "_sheet.png"
+        sprite_path = "sprites/" .. folder .. "/" .. obj_def["id"] .. "_sheet.png"
         table.insert({obj_def, sprite_path})
         end
     return variation_list
@@ -53,4 +53,17 @@ function define_my_furniture()
     my_furniture_definition = {"object definition here"}
     tooltip = {preface = "This is a", suffix = "piece of furniture by Me"}
     define_from_variation_list(make_variated(my_furniture_definition, COLORS, "my_furniture", 1, tooltip))
+    api_define_object(my_furniture_definition, sprite_path(my_furniture_definition["id"])) --also define non-variated object.
+end
+
+
+--- Generates and returns a path to the sprite of an object as a string
+---@param name string typically object_definition["id"]
+---@param folder string (Optional) Subfolder for the sprite, if none is given will use name instead.
+function sprite_path(name, folder) 
+    if type(folder) == "string" then
+        return "sprites/" .. folder .. "/" .. name .. "_sheet.png"
+    else
+        return "sprites/" .. name "/" .. name .. "_sheet.png"
+    end
 end
